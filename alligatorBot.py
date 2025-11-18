618815eb03b1db4481c2f259088381f01a7bf97f
pavel <.email@zalupa.com>
2025-11-18 18:54:53 +0300

    fix acc

--- alligatorBot.py
+++ alligatorBot.py
@@ -15,8 +15,7 @@ from authenticator import MT5Auth
 from history import History
 
 # Конфигурация бота
-TOKEN = "8062299925:AAFA14ISWThGN9D0ktg7lXxRtX2lvglzG9w" #AllogatorMinerTest2
-#TOKEN = "8235563483:AAF1gESvpoES7ttfJBlOKMfbA2z7BVl55LA" #AlligatorMinerTest
+TOKEN = Account.token
 CHAT_ID = None  # Будет заполнено после /start
 # Белый список разрешенных пользователей
 ALLOWED_USERS = {
@@ -24,7 +23,7 @@ ALLOWED_USERS = {
     # "987654321": "Другой пользователь",  # Можно добавить других
 }
 
-account = Account.accountDemo2
+account = Account.account
 auth = MT5Auth(account)
 auth.login()
 trading = Trading()
